a19fef954c2f7bdb7a090f453d5cedaf91eecaca
Luwam <luwamtmariam@gmail.com>
2023-04-09 00:04:04 -0500

    initial files

---
+++ src/test/java/springreactaws/SpringReactAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package springreactaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
